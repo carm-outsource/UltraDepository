52a0d282494bd183c94c37efa26ffa521d8a95c8
CarmJos <KarmunJ@outlook.com>
2022-01-05 10:44:50 +0800

    [v1.1.7] 使用自定义存储却未重写方法时输出提示

--- src/main/java/cc/carm/plugin/ultradepository/storage/impl/CustomStorage.java
+++ src/main/java/cc/carm/plugin/ultradepository/storage/impl/CustomStorage.java
@@ -1,5 +1,6 @@
 package cc.carm.plugin.ultradepository.storage.impl;
 
+import cc.carm.plugin.ultradepository.UltraDepository;
 import cc.carm.plugin.ultradepository.data.UserData;
 import cc.carm.plugin.ultradepository.storage.DataStorage;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +14,10 @@ public class CustomStorage implements DataStorage {
 	@Override
 	@TestOnly
 	public boolean initialize() {
+		UltraDepository.getInstance().error("您选择使用自定义存储，但并没有应用成功。");
+		UltraDepository.getInstance().error("请访问 https://github.com/CarmJos/UltraDepository/blob/master/.documentation 获取相关帮助！");
+		UltraDepository.getInstance().error("You are using CustomStorage, but not overwrite the methods.");
+		UltraDepository.getInstance().error("Please view https://github.com/CarmJos/UltraDepository/blob/master/.documentation to get more information.");
 		return false;
 	}
 
@@ -33,5 +38,5 @@ public class CustomStorage implements DataStorage {
 	public void saveUserData(@NotNull UserData data) {
 
 	}
-	
+
 }
