d52e001e7c2d82e05a0903485d14f53412ec43c9
carm <Karmun@foxmail.com>
2022-01-24 01:24:18 +0800

    [v1.3.8] 修复更新提示类型错误的问题

--- src/main/java/cc/carm/plugin/ultradepository/hooker/UpdateChecker.java
+++ src/main/java/cc/carm/plugin/ultradepository/hooker/UpdateChecker.java
@@ -19,10 +19,10 @@ public class UpdateChecker {
 				plugin.error("检查更新失败，请您定期查看插件是否更新，避免安全问题。");
 				plugin.error("下载地址 " + downloadURL);
 			} else if (behindVersions == 0) {
-				plugin.print("检查完成，当前已是最新版本。");
+				plugin.log("检查完成，当前已是最新版本。");
 			} else if (behindVersions > 0) {
-				plugin.print("发现新版本! 目前已落后 " + behindVersions + " 个版本。");
-				plugin.print("最新版下载地址 " + downloadURL);
+				plugin.log("发现新版本! 目前已落后 " + behindVersions + " 个版本。");
+				plugin.log("最新版下载地址 " + downloadURL);
 			} else {
 				plugin.error("检查更新失败! 当前版本未知，请您使用原生版本以避免安全问题。");
 				plugin.error("最新版下载地址 " + downloadURL);
