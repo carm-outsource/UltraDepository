baeee39d075dfb416d045fa412843010b5607e56
carm <Karmun@foxmail.com>
2022-01-23 11:16:44 +0800

    [v1.3.6] 修改更新介绍

--- src/main/java/cc/carm/plugin/ultradepository/hooker/UpdateChecker.java
+++ src/main/java/cc/carm/plugin/ultradepository/hooker/UpdateChecker.java
@@ -13,16 +13,19 @@ public class UpdateChecker {
 					plugin.getDescription().getVersion()
 			);
 
+			String downloadURL = GithubReleases4J.getReleasesURL("CarmJos", "UltraDepository");
+
 			if (behindVersions == null) {
 				plugin.error("检查更新失败，请您定期查看插件是否更新，避免安全问题。");
-				plugin.error("插件下载地址&e " + GithubReleases4J.getReleasesURL("CarmJos", "UltraDepository"));
+				plugin.error("下载地址 " + downloadURL);
+			} else if (behindVersions == 0) {
+				plugin.error("检查完成，当前已是最新版本。");
+			} else if (behindVersions > 0) {
+				plugin.error("发现新版本! 目前已落后 " + behindVersions + " 个版本。");
+				plugin.error("最新版下载地址 " + downloadURL);
 			} else {
-				if (behindVersions > 0) {
-					plugin.log("检查更新完成，当前已落后 " + behindVersions + " 个版本。");
-					plugin.log("最新版本下载地址&e " + GithubReleases4J.getLatestReleaseURL("CarmJos", "UltraDepository"));
-				} else {
-					plugin.log("检查更新完成，当前已是最新版本。");
-				}
+				plugin.error("检查更新失败! 当前版本未知，请您使用原生版本以避免安全问题。");
+				plugin.error("最新版下载地址 " + downloadURL);
 			}
 
 		});
